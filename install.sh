fac094a8953dc2c19bc828e8fadc7765f73d28fb
jasonBrom <60287510+jasonBrom@users.noreply.github.com>
2020-01-26 15:03:51 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,17 @@
 echo "开始安装....."
 yum install git -y
 yum install htop -y
+yum -y update
+yum install gcc libcap libpcap libpcap-devel screen php dstat cmake gmp gmp-devel gengetopt byacc flex git json-c zmap iptraf nano -y
+yum groupinstall "Development Tools" -y
+yum install gcc php-devel php-pear libssh2 libssh2-devel -y
+pecl install -f ssh2
+
+
+
+
+
+touch /etc/php.d/ssh2.ini
+echo extension=ssh2.so > /etc/php.d/ssh2.ini
+Service httpd restart
+chkconfig httpd on
