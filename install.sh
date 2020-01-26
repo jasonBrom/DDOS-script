b7fa7425b64d4dd6459d93dc8900716261e2c3e9
jasonBrom <60287510+jasonBrom@users.noreply.github.com>
2020-01-26 19:53:30 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 echo "开始安装....."
 yum install git -y
+yum install python -y
 yum install htop -y
 yum -y update
 yum install gcc libcap libpcap libpcap-devel screen php dstat cmake gmp gmp-devel gengetopt byacc flex git json-c zmap iptraf nano -y
@@ -17,3 +18,8 @@ touch /etc/php.d/ssh2.ini
 echo extension=ssh2.so > /etc/php.d/ssh2.ini
 Service httpd restart
 chkconfig httpd on
+wget https://raw.githubusercontent.com/0x01h/pyddoz/master/requirements.txt
+wget https://raw.githubusercontent.com/0x01h/pyddoz/master/pyddoz.py
+wget https://raw.githubusercontent.com/0x01h/pyddoz/master/proxify.py
+wget https://raw.githubusercontent.com/0x01h/pyddoz/master/.travis.yml
+wget https://raw.githubusercontent.com/0x01h/pyddoz/master/.gitignore
